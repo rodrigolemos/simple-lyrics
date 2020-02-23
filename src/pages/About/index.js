9c16cdd0dc201892f4a43125f9cb7571628cf961
Rodrigo <rodrigolemosrl@hotmail.com>
2020-02-22 23:07:59 -0300

    Updating disclaimer

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -10,8 +10,7 @@ export default class About extends Component {
                     <Title>Hello world! <FaHandPeace/></Title>
                     <p>
                         Este app tem o intuito de demonstrar a utilização prática de uma API externa para consulta e exibição de imagens, nomes e letras de músicas e bandas.
-                        Como aplicação de estudos, não há intenção de comercialização de informações nem de quaisquer ações que possam infringir os direitos autorais dos artistas
-                        nem do Vagalume, fornecedor da API.
+                        Como aplicação de estudos, não há intenção de infringir os direitos autorais dos artistas nem do Vagalume, fornecedor da API.
                     </p>
                     <MyButton to="/app">Ok, continuar!</MyButton>
                 </Disclaimer>
